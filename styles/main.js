ae9f44c6421ded17bec7d0e9f57bb6152c9a63eb
AlexASC <41082381+AlexASC@users.noreply.github.com>
2018-08-16 11:43:46 -0400

    Update main.js

--- styles/main.js
+++ styles/main.js
@@ -1,22 +1,3 @@
-const database = firebase.database().ref()
-
-function updateDB(event) {
-    event.preventDefault();
-    const username = usernameElement.value;
-    const message = messageElement.value;
-
-    usernameElement.value = "";
-    messageElement.value = "";
-
-    console.log(username + ' : ' + message)
-
-    const value = {
-        NAME:username,
-        MESSAGE:message
-    }
-    database.push(value)
-}  
-
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
